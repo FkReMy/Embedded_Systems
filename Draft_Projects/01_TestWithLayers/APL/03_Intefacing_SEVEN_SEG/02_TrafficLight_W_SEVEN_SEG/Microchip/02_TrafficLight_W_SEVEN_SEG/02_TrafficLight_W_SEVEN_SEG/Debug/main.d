90f3b0da93f18a2e84a24257f384c33e2469d6ed
FkReMy <fekryjunior@gmail.com>
2024-05-12 20:29:41 +0300

    02_TrafficLight_W_SEVEN_SEG Update

--- Draft_Projects/01_TestWithLayers/APL/03_Intefacing_SEVEN_SEG/02_TrafficLight_W_SEVEN_SEG/Microchip/02_TrafficLight_W_SEVEN_SEG/02_TrafficLight_W_SEVEN_SEG/Debug/main.d
+++ Draft_Projects/01_TestWithLayers/APL/03_Intefacing_SEVEN_SEG/02_TrafficLight_W_SEVEN_SEG/Microchip/02_TrafficLight_W_SEVEN_SEG/02_TrafficLight_W_SEVEN_SEG/Debug/main.d
@@ -5,9 +5,6 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  .././SEVEN_SEG_interface.h .././LED_interface.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
@@ -21,12 +18,6 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
 
 .././SEVEN_SEG_interface.h:
 
--- Draft_Projects/01_TestWithLayers/APL/03_Intefacing_SEVEN_SEG/02_TrafficLight_W_SEVEN_SEG/Microchip/02_TrafficLight_W_SEVEN_SEG/02_TrafficLight_W_SEVEN_SEG/main.c
+++ Draft_Projects/01_TestWithLayers/APL/03_Intefacing_SEVEN_SEG/02_TrafficLight_W_SEVEN_SEG/Microchip/02_TrafficLight_W_SEVEN_SEG/02_TrafficLight_W_SEVEN_SEG/main.c
@@ -6,7 +6,6 @@
 
 #define F_CPU 8000000UL
 #include <util/delay.h>
-#include <stdio.h>
 
 #include "SEVEN_SEG_interface.h"
 #include "LED_interface.h"
@@ -17,6 +16,7 @@ int main(void)
 	SEVEN_SEG_Init(portd);
 	//Units
 	SEVEN_SEG_Init(portc);
+	
 	//Red
 	LED_Init(porta,pin0);
 	//Yellow
@@ -26,55 +26,53 @@ int main(void)
 	
     while (1) 
     {
+
 		unsigned char red = 9;
 		unsigned char yellow = 5;
 		unsigned char green = 8;
 		
-		if ( red == 9 && yellow == 5 && green == 8)
-		{
-			//red
-			for(int i = red; i >= 0; i--)
-			{	
-				LED_Off(porta,pin2);
-				LED_On(porta,pin0);
+		//red
+		for(int i = red; i >= 0; i--)
+		{	
+			LED_Off(porta,pin2);
+			LED_On(porta,pin0);
 				
+			SEVEN_SEG_Write(portc,i);
+			SEVEN_SEG_Write(portd,i);
+			_delay_ms(1000);
+				
+			//yellow
+			if (i == 0)
+			{
+				//red at 0
 				SEVEN_SEG_Write(portc,i);
 				SEVEN_SEG_Write(portd,i);
-				_delay_ms(1000);
-				
-				//yellow
-				if (i == 0)
-				{
-					//red at 0
-					SEVEN_SEG_Write(portc,i);
-					SEVEN_SEG_Write(portd,i);
 					
-					for (int j = yellow; j>=0; j--)
-					{
-						LED_Off(porta,pin0);
-						LED_On(porta,pin1);
+				for (int j = yellow; j>=0; j--)
+				{
+					LED_Off(porta,pin0);
+					LED_On(porta,pin1);
 						
+					SEVEN_SEG_Write(portc,j);
+					SEVEN_SEG_Write(portd,j);
+					_delay_ms(1000);
+						
+					//green
+					if (j == 0)
+					{
+						//yellow at 0
 						SEVEN_SEG_Write(portc,j);
 						SEVEN_SEG_Write(portd,j);
-						_delay_ms(1000);
-						
-						//green
-						if (j == 0)
-						{
-							//yellow at 0
-							SEVEN_SEG_Write(portc,j);
-							SEVEN_SEG_Write(portd,j);
 							
-							for (int k = green; k >=0; k--)
-							{
-								LED_Off(porta,pin1);
-								LED_On(porta,pin2);
+						for (int k = green; k >=0; k--)
+						{
+							LED_Off(porta,pin1);
+							LED_On(porta,pin2);
 								
-								SEVEN_SEG_Write(portd,k);
-								SEVEN_SEG_Write(portc,k);
-								_delay_ms(1000);
+							SEVEN_SEG_Write(portd,k);
+							SEVEN_SEG_Write(portc,k);
+							_delay_ms(1000);
 								
-							}
 						}
 					}
 				}
